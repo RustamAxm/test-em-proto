6d41a125a351855e6db9c367583effe0c6efca8b
rustam <rustamaxm@gmail.com>
2025-10-02 14:07:01 +0300

    fix Cmake and absl find

--- desktop/main.cpp
+++ desktop/main.cpp
@@ -1,12 +1,14 @@
 //
 // Created by rustam on 04.08.24.
 //
-#include "cpp_gen/uart_msg.pb.h"
+#include "uart_msg.pb.h"
 #include <iostream>
 
 int main() {
     auto acc = Accelerometer();
     acc.set_x(12);
-    std::cout << acc.x() << std::endl;
+    acc.add_payload(34);
+    acc.add_payload(45);
+    std::cout << acc.x() << " " << acc.payload()[0] << std::endl;
     return 0;
 }
